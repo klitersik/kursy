669445e0934f384dc056065c565ed7073e322105
klitersik <46646404+klitersik@users.noreply.github.com>
2023-11-22 15:59:41 +0100

    Update app.py

--- app.py
+++ app.py
@@ -28,15 +28,13 @@ df = get_data()
 
 with st.sidebar:
     days_number = st.slider("Wybierz ilość dni",2,20)
-    volume_number = st.slider("Wybierz minimalną wielkość wolumenu braną pod uwagę",500_000,300_000_000,10000)
+    volume_number = st.number_input('Minimalny Wolumen',key="3",min_value=100_000,step=1000)
     min_value = st.number_input('Wybierz minimalną wartość Kursu',key="1",step=0.1)
-    max_value = st.number_input('Wybierz maksymalną wartość Kursu',key="2",min_value=min_value+0.1,step=0.1)
+    max_value = st.number_input('Wybierz maksymalną wartość Kursu',key="2",min_value=min_value,step=0.1)
     start_date = datetime.now().date()
     end_date = start_date - timedelta(days=days_number)
 
 df_completed = df[(df['Data'] <= start_date) & (df['Data'] >= end_date)]
-df_completed = df_completed[(df['Kurs'] <= max_value) & (df['Kurs'] >= min_value)]
-df_completed = df_completed[(df['Wolumen'] >= volume_number)]
 
 if days_number is not None and not df_completed.empty:
     # Sortowanie danych po dacie
@@ -49,37 +47,49 @@ if days_number is not None and not df_completed.empty:
         first_volume = group.iloc[0]['Wolumen']
         last_volume = group.iloc[-1]['Wolumen']
         zmiana_wolumenu = (last_volume - first_volume)/last_volume
-        new_data.append({'Nazwa': nazwa, 'zmiana_wolumenu': zmiana_wolumenu})
+        max_date_row = group[group['Data'] == group['Data'].max()]  # Wiersz z maksymalną datą
+        if not max_date_row.empty:
+            kurs = max_date_row.iloc[0, -2]
+            wolumen = max_date_row.iloc[0, -3]
+            new_data.append({
+                'Nazwa': nazwa,
+                'Zmiana_wolumenu': zmiana_wolumenu,
+                'Kurs': kurs,
+                'Wolumen': wolumen
+            })
 
     # Tworzenie nowego DataFrame
     new_df = pd.DataFrame(new_data)
 
     # Sortowanie malejąco po zmianie wolumenu
-    new_df = new_df.sort_values(by='zmiana_wolumenu', ascending=False)
+    new_df = new_df.sort_values(by='Zmiana_wolumenu', ascending=False)
 
     # Obliczenie średniego wzrostu dla wszystkich nazw
-    sredni_wzrost = new_df['zmiana_wolumenu'].mean()
+    sredni_wzrost = new_df['Zmiana_wolumenu'].mean()
 
-    col1, col2, col3 = st.columns(3)
+    new_df = new_df[(new_df['Kurs'] <= max_value) & (df['Kurs'] >= min_value)]
+    new_df = new_df[(new_df['Wolumen'] >= volume_number)]
+
+    col1, col2, col3, col4 = st.columns(4)
     col1.metric("Sprawdzane dni", days_number)
-    col2.metric("Przedział cenowy", f"{min_value:.2f}$ - {max_value:.2f}$")
+    col2.metric("Przedział cenowy", f"{min_value:.2f}-{max_value:.2f}")
     col3.metric("Średni wzrost", f"{sredni_wzrost:.2f}")
-
+    col4.metric("Minimalny wolumen", f"{volume_number}")
     #st.write(new_df)
 
     if not new_df.empty:
-        df_positve = new_df[(new_df['zmiana_wolumenu'] > 0)]
+        df_positve = new_df[(new_df['Zmiana_wolumenu'] > 0)]
         st.header(f"Zmiana wolumenu > 0, {len(df_positve)} uniklanych wartośći")
         st.dataframe(df_positve)
-    
+
         st.divider()
-    
-        df_zero = new_df[(new_df['zmiana_wolumenu'] == 0)]
+
+        df_zero = new_df[(new_df['Zmiana_wolumenu'] == 0)]
         st.header(f"Zmiana wolumenu = 0, {len(df_zero)} uniklanych wartośći")
         st.dataframe(df_zero)
-    
+
         st.divider()
-    
-        df_negative = new_df[(new_df['zmiana_wolumenu'] < 0)]
+
+        df_negative = new_df[(new_df['Zmiana_wolumenu'] < 0)]
         st.header(f"Zmiana wolumenu < 0, {len(df_negative)} uniklanych wartośći")
         st.dataframe(df_negative)
